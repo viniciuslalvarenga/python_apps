39dd1c81f3ae21f3f0204fa4adcf1424b88bb76f
Vinicius Alvarenga <viniciusl.alvarenga@hotmail.com>
2024-04-18 21:46:52 +0100

    add comment remember useful

--- proj1_ToDoList/gui.py
+++ proj1_ToDoList/gui.py
@@ -5,6 +5,12 @@ label = PySimpleGUI.Text("Type in a to-do")
 input_box = PySimpleGUI.InputText(tooltip="Enter todo")
 add_button = PySimpleGUI.Button("Add")
 
-window = PySimpleGUI.Window("My To-Do App",layout=[[label], [input_box, add_button]])
+window = PySimpleGUI.Window("My To-Do App", \
+    layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
+
+
+#remember useful
+#comb = PySimpleGUI.Combo(["valor1","valor2"],default_value="unset",size=10)
+#c_box = PySimpleGUI.Checkbox("istrue")
