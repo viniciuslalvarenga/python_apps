acd5173995fe3bea7ab8dbe093dd0a41e5921d8d
Vinicius Alvarenga <viniciusl.alvarenga@hotmail.com>
2024-04-29 09:01:16 +0100

    Implement complete ans Exit button

--- proj1_ToDoList/gui.py
+++ proj1_ToDoList/gui.py
@@ -13,8 +13,14 @@ list_box = PySimpleGUI.Listbox(values=functions.get_todos(),
                                 key="todos", enable_events=True, size=[45, 10])
 edit_button = PySimpleGUI.Button("Edit")
 
+complete_button = PySimpleGUI.Button("Complete")
+
+exit_button = PySimpleGUI.Button("Exit")
+
 window = PySimpleGUI.Window("My To-Do App", 
-    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+    layout=[[label], [input_box, add_button],
+            [list_box, edit_button, complete_button],
+            [exit_button]],
     font=("Helvetica",12))
 
 while True:
@@ -32,6 +38,7 @@ while True:
             todos.append(new_todos)
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
             
         case "Edit":
             todo_to_edit = values["todos"][0]
@@ -42,6 +49,18 @@ while True:
             todos[index] = new_todos + "\n"
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
+            
+        case "Complete":
+              todo_to_complete = values["todos"][0]
+              todos = functions.get_todos()
+              todos.remove(todo_to_complete)
+              functions.write_todos(todos)
+              window["todos"].update(values=todos)
+              window["todo"].update(value="")
+            
+        case "Exit":
+            break
         
         #When we select an item in the list box, it generate an event with the
         #list_box key value
